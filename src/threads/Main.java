a81858619e37fc28de4a9be76805e2ca8b8a21dc
yagray <yagray@mail.ru>
2019-01-14 19:03:17 +0300

    Работает с любым количеством потоков

--- src/threads/Main.java
+++ src/threads/Main.java
@@ -4,51 +4,92 @@ import java.util.ArrayList;
 
 public class Main {
 
-    static final int size = 200;
-    static final int h = size / 2;
+    static final int ARRAY_SIZE = 10000000;
+    static float[] dataArray = new float[ARRAY_SIZE];
 
-    static int[] dataArray = new int[size];
+    static final int ONE_THREAD = 1;
+    static final int MULTI_THREADS = 3;
 
+    /**
+     * TODO: Точка входа
+     */
     public static void main(String[] args) {
-        System.out.println("Hello");
 
         // Инициализация массива
-        for(int i = 0; i < size; i++){
-            dataArray[i] = 1;
+        Main.initData();
+
+        // Вычисляем в один поток
+        long millis = System.currentTimeMillis();
+        calculateIn(ONE_THREAD);
+        System.out.println("Result: threads " + ONE_THREAD + ", duration " + (System.currentTimeMillis() - millis) + " ms\n");
+
+        // Вычисляем в несколько потоков
+        Main.initData();
+        millis = System.currentTimeMillis();
+        calculateIn(MULTI_THREADS);
+        System.out.println("Result: threads " + MULTI_THREADS + ", duration " + (System.currentTimeMillis() - millis) + " ms\n");
+    }
+
+    /**
+     * TODO: Сгенерить требуемое кол-во потоков. Если потоков несколько, то выдать каждому
+     * TODO: копию части исходного массива для вычислений.
+     *
+     * @param threadsQty Количество потоков вычисления
+     */
+    public static void calculateIn(int threadsQty){
+
+        if(threadsQty == 1) {
+            // Фабрика генерит один поток без создания копии основного массива
+            Thread t = new ThreadFabric(dataArray, threadsQty).generateNoCopy();
+            System.out.println("Created 1 thread");
+            t.start();
+
+            try{
+                t.join();
+            } catch (InterruptedException e) {}
         }
+        else {
+            // Фабрика генерит несколько потоков, делая копию части исходного массива
+            Thread[] threads = new ThreadFabric(dataArray, threadsQty).generateWithCopy();
+            System.out.println("Created " + threads.length + " threads");
 
-        // Фабрика потоков
-//        ThreadFabric tf = new ThreadFabric(size, 7);
-        ThreadFabric tf = new ThreadFabric(size, 17);
-        Thread[] threads = tf.generate();
+            // Стартуем потоки
+            for(Thread t : threads) t.start();
 
-        System.out.println("Created " + threads.length + " threads");
+            // Ждем завершения
+            try {
+                for (Thread t : threads) t.join();
 
-        for(Thread t : threads) t.start();
+                // Собрать результирующий массив
+                int destPos = 0;
+                for(int i = 0; i < threads.length; i++){
+                    Calculator calc = (Calculator)threads[i];
+                    System.arraycopy(calc.getArray(), 0, dataArray, destPos, calc.getLength());
+                    destPos += calc.getLength();
+                }
+            } catch (InterruptedException e) {}
+        }
+    }
 
-        try {
-            for (Thread t : threads) t.join();
-
-            for (int i = 0; i < dataArray.length; i++){
-                System.out.println(Main.dataArray[i] + "\n");
-            }
-
-        } catch (InterruptedException e) {}
+    /**
+     * Инициализация исходного массива
+     */
+    static void initData() {
+        for(int i = 0; i < Main.dataArray.length; i++) Main.dataArray[i] = 1;
     }
 }
-
-
 
 /**
  *
  */
-
 class ThreadFabric {
-    private final int arraySize;
+    private final int sourceDataLength;
+    private float[] sourceData;
     private final int threads;
 
-    public ThreadFabric(int arraySize, int threads) {
-        this.arraySize = arraySize;
+    public ThreadFabric(float[] sourceData, int threads) {
+        this.sourceDataLength = sourceData.length;
+        this.sourceData = sourceData;
         this.threads = threads;
     }
 
@@ -58,42 +99,59 @@ class ThreadFabric {
      * равное остатку от деления равномерно "размазываем" между потоками. В результате
      * некторомы потокам придется обрабатывать на один элемент массива больше.
      */
-    Thread[] generate(){
-        int subArraySize = arraySize / threads;
-        int remainder = arraySize % threads;
+    Thread[] generateWithCopy() {
+        int subArraySize = sourceDataLength / threads;
+        int remainder = sourceDataLength % threads;
         int index = 0;
 
         ArrayList<Thread> al = new ArrayList<>();
 
         for(int i = 0; i < threads; i++) {
             int length = (i < remainder) ?  (subArraySize  + 1) : subArraySize;
-            al.add(new Calculator(Integer.toString(i), index, length));
+
+            float[] subArray = new float[length];
+            System.arraycopy(sourceData, index, subArray, 0, length);
+
+            al.add(new Calculator(Integer.toString(i), subArray));
             index += length;
         }
-
         return al.toArray(new Thread[al.size()]);
     }
 
+    Thread generateNoCopy() {
+        return new Calculator("single", sourceData);
+    }
 }
 
+/**
+ * Поток вычислений
+ */
 class Calculator extends Thread {
-    private int indexStart;
-    private int qty;
+    private final int length;
+    private final float[] array;
 
-    public Calculator(String name, int indexStart, int qty) {
+    public Calculator(String name, float[] array) {
         super(name);
-        this.indexStart = indexStart;
-        this.qty = qty;
+        this.array = array;
+        this.length = array.length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public float[] getArray() {
+        return array;
     }
 
     @Override
     public void run() {
-        System.out.println("Thread " + this.getName() + ", startIndex=" + indexStart + ", range=" + qty);
-        for(int i = indexStart; i < (indexStart + qty); i++) {
-            Main.dataArray[i] += 1;
-        }
+        System.out.println("Thread " + this.getName() + " started. Array length " + length);
+        for(int i = 0; i < length; i++) mathProc(i);
     }
 
-
+    private void mathProc(int i) {
+        array[i] = (float)(array[i] * Math.sin(0.2f + i/5) * Math.cos(0.2f + i/5) * Math.cos(0.4f + i/2));
+    }
 }
 
